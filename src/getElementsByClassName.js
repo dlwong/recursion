f431e14eed71c69daeb22215496c6df7b3b4f8fc
Dorris Wong <pingki@sbcglobal.net>
2019-05-16 21:56:08 -0700

    implemented getElementsByClassName.js and stringifyJSON.js

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,40 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+
+
+//The getElementsByClassName() method returns a collection of all elements 
+//in the document with the specified class name, as a NodeList object.
+//The NodeList object represents a collection of nodes. The nodes can be accessed 
+//by index numbers. The index starts at 0.
+
+
+//The getElementsByClassName() method returns a collection of an element's
+// child elements with the specified class name, as a NodeList object.
+
+//You should use document.body, element.childNodes, and element.classList
+
+//The classList property returns the class name(s) of an element
+
+//The childNodes property returns a collection of a node's child nodes, 
+//as a NodeList object.
+
+var getElementsByClassName = function(className) {
+    var elArray = [];  
+
+    function searchChild(searchNode){
+        //check if it has classnames and contains our target classname
+        if(searchNode.classList && searchNode.classList.contains(className)){
+            elArray.push(searchNode) //add to array
+        }
+        if(searchNode.children.length>0){ //if it has children
+            _.map(searchNode.children, function(el){ 
+                searchChild(el); //recursively search
+            })
+        }
+     }
+
+     searchChild(document.body) //starting from the parent
+     return elArray;
 };
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,45 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
-};
+  if (typeof obj === 'number') {
+    return ""+obj;
+  }else if (obj === null) {
+    return ""+null;
+  }else if (typeof obj === 'string'){
+    return "\""+obj+"\"";
+  }else if(typeof obj === 'boolean'){
+    return ""+obj;
+  }else if (Array.isArray(obj)){
+    let stringArray = '['
+
+    if(obj.length>0){
+       _.each(obj, function(x){
+          stringArray += stringifyJSON(x);
+
+          if(obj.length>1){
+            stringArray += ','
+          }
+       })
+    if(obj.length>1){
+       stringArray = stringArray.slice(0,stringArray.length-1)
+       }
+    }
+    return stringArray += ']';
+  }else if(typeof obj === "object") {
+    if(Object.keys(obj).length === 0){
+      return '{}';
+    }
+
+    let stringObj = '{'; 
+
+    for(var k in obj) {
+        if(typeof obj[k] === 'function') {
+          return stringObj+'}'
+        }else{
+    	stringObj += stringifyJSON(k) + ':' + stringifyJSON(obj[k]) + ",";
+        }
+    }
+    return stringObj.slice(0, stringObj.length - 1) + '}'
+
+  } 
+}
